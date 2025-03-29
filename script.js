adcdc45f4c490c5be4ae2b9fa3543e7de3a6d61b
[angie-0407] <[angie.garcia@est.ulsa.edu.ni]>
2025-03-29 11:46:19 -0600

    Cuadros de texto aceptan ingresar numeros

---
+++ script.js
@@ -0,0 +1,25 @@
+const billInput = document.querySelector(".bill-input");
+const peopleInput = document.querySelector(".people-input");
+const tipPerPerson = document.getElementById("tip-amount");
+const totalPerPerson = document.getElementById("total-amount");
+
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+
+billInput.value ="0.0";
+peopleInput.value = "1";
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+
+let billValue = 0.0;
+let peopleValue = 1;
+
+function billInputFun (){
+    billValue = parseFloat(billInput.value);
+    calculateTip();
+}
+
+function peopleInputFun (){
+    peopleValue = parseFloat(peopleInput.value);
+    console.log(peopleValue);
+}
